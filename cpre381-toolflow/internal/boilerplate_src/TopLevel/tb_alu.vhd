c2d21c9fc48c2c0f2203a5924e5c9234a1bd5e97
zrhirst <zrhirst@vlinux-35.ece.iastate.edu>
2022-03-29 16:26:07 -0500

    this vdi is dunmb

--- cpre381-toolflow/internal/boilerplate_src/TopLevel/tb_alu.vhd
+++ cpre381-toolflow/internal/boilerplate_src/TopLevel/tb_alu.vhd
@@ -1,1 +1,15 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
 
+entity tb_alu is 
+generic(gCLK_HPER   : time := 10 ns;
+	N: integer := 32);   
+end tb_alu;
+
+architecture mixes of tb_alu is 
+
+component 
+
+
+
+end tb_alu;
