eec9d75e03529426d70dfd386c4ff89c94988fde
zrhirst <zrhirst@vlinux-35.ece.iastate.edu>
2022-03-29 16:32:46 -0500

    done losing data

--- cpre381-toolflow/internal/boilerplate_src/TopLevel/tb_alu.vhd
+++ cpre381-toolflow/internal/boilerplate_src/TopLevel/tb_alu.vhd
@@ -8,7 +8,35 @@ end tb_alu;
 
 architecture mixes of tb_alu is 
 
-component 
+component ALU is 
+	port(	read_data_1: in std_logic_vector(N-1 downto 0);
+		read_data_2: in std_logic_vector(N-1 downto 0);
+		alu_control: in std_logic_vector(5 downto 0);
+		shampt: in std_logic_vector(4 downto 0);
+		output: out std_logic_vector(N-1 downto 0); 
+		overflow: out std_logic; 
+		carryout: out std_logic; 
+		zero: out std_logic); 
+end component; 
+
+signal s_rd1, s_rd2, s_output: std_logic_vector(N-1 downto 0);
+signal s_control: std_logic_vector(5 downto 0);
+signal s_shampt: std_logic_vector(4 downto 0); 
+signal s_overflow, s_carryout, s_zero: std_logic;
+
+begin 
+
+mapp:ALU
+port map(
+	read_data_1 => s_rd1,
+	read_data_2 => s_rd2,
+	alu_control => s_control,
+	shampt => s_shampt,
+	output => s_output,
+	overflow => s_overflow,
+	carryout => s_carryout,
+	zero => s_zero
+);
 
 
 
