257edef57a59906686255483e0c51bfc851d971e
zrhirst <zrhirst@vlinux-07.ece.iastate.edu>
2022-04-08 22:56:57 -0500

    Implemented the control logic testing for the processor. Need to debug and continue to add test cases

---
+++ cpre381-toolflow/proj/mips/Proj1_cf_tests.s
@@ -0,0 +1,28 @@
+#
+# Logical Jump Tests 
+#
+
+# data section
+.data
+
+# code/instruction section
+.text
+
+jal dumb
+ori $s3 $zero 0x1234
+beq $s1, 1, jeffspizza #issue here
+#bne $s1, 0, urmom
+j exit
+
+urmom:
+    jr $ra
+
+jeffspizza:
+    jr $ra
+
+dumb:
+    addi $s1, $0, 1
+    jr $ra
+
+exit:
+    halt
