eb263f326798d30c4177eb76ababb9efd25ee6a7
cthomp01 <cthomp01@co2050-02.ece.iastate.edu>
2022-05-04 17:40:35 -0500

    Change NOP
    Change the NOP to add because we don't know if NOP is supported.

--- MultiCycleSoftwareScheduled/proj/mips/Proj1_base_test.s
+++ MultiCycleSoftwareScheduled/proj/mips/Proj1_base_test.s
@@ -9,42 +9,42 @@ addi  $3,  $0,  3	# Place 3 in $3 | 3
 addi  $4,  $0,  4	# Place 4 in $4 | 4
 addi  $5,  $0,  5	# Place 5 in $5 | 5
 addi  $6,  $0,  6	# Place 6 in $6 | 6
-nop
-nop # RAW hazard
+add $0, $0, $0
+add $0, $0, $0 # RAW hazard
 
 #add
 add $11, $6, $5     # $11 = $6 + $5 | 11
 add $12, $4, $3     # $12 = $4 + $3 | 7
 add $13, $2, $1     # $13 = $2 + $1 | 3
-nop # RAW hazard $12
+add $0, $0, $0 # RAW hazard $12
 add $14, $11, $12   # $14 = $11 + $12 | 18
-nop
-nop # RAW hazard
+add $0, $0, $0
+add $0, $0, $0 # RAW hazard
 add $15, $14, $13   # $15 = $14 + $13 | 21
 
 #sub
 sub $7, $6, $1      # $6 - $1 in $7 | 5
 sub $8, $5, $2      # $5 - $2 in $8 | 3
 sub $9, $4, $3      # $4 - $3 in $9 | 1
-nop
-nop # RAW hazard
+add $0, $0, $0
+add $0, $0, $0 # RAW hazard
 sub $10, $9, $zero  # $9 - $zero in $10 | 1
 
 #slt
 slt $1, $1, $2      # $1 < $2 | 1
-nop
-nop # WAR hazard (might need 1)
+add $0, $0, $0
+add $0, $0, $0 # WAR hazard (might need 1)
 slt $2, $4, $3      # $4 < $3 | 0
-nop
-nop # WAR hazard
+add $0, $0, $0
+add $0, $0, $0 # WAR hazard
 slt $3, $5, $6      # $5 < $6 | 1
 
 #slti
 slti $1, $1, 5      # $1 < 5 | 1
 slti $2, $4, 2      # $4 < 2 | 0
 slti $3, $5, 14     # $5 < 14 | 1
-nop
-nop # RAW hazard
+add $0, $0, $0
+add $0, $0, $0 # RAW hazard
 
 #and
 and $1, $1, $3
@@ -79,51 +79,51 @@ nor $3, $5, $5
 #xor
 xor $2, $zero, $4
 xor $3, $3, $4
-nop
-nop # WAR hazard
+add $0, $0, $0
+add $0, $0, $0 # WAR hazard
 xor $3, $s2, $s1
 
 #xori
 xori $2, $2, 1
-nop
-nop # RAW
+add $0, $0, $0
+add $0, $0, $0 # RAW
 xori $2, $2, 0
-nop
-nop # WAR
+add $0, $0, $0
+add $0, $0, $0 # WAR
 xori $2, $zero, 0
-nop
-nop # RAW
+add $0, $0, $0
+add $0, $0, $0 # RAW
 
 #sll
 sll $2, $2, 8
-nop
-nop # RAW
+add $0, $0, $0
+add $0, $0, $0 # RAW
 sll $2, $2, 1
 sll $3, $3, 4
-nop
-nop # WAR
+add $0, $0, $0
+add $0, $0, $0 # WAR
 
 #srl
 srl $3, $2, 1
-nop
-nop # RAW
+add $0, $0, $0
+add $0, $0, $0 # RAW
 srl $3, $3, 5
-nop
-nop # WAR
+add $0, $0, $0
+add $0, $0, $0 # WAR
 srl $3, $2, 8
-nop
-nop # RAW
+add $0, $0, $0
+add $0, $0, $0 # RAW
 
 #sra
 sra $3, $3, 1
-nop
-nop # WAR
+add $0, $0, $0
+add $0, $0, $0 # WAR
 sra $3, $2, 8
-nop
-nop # RAW
+add $0, $0, $0
+add $0, $0, $0 # RAW
 sra $2, $3, 4
-nop
-nop # WAR
+add $0, $0, $0
+add $0, $0, $0 # WAR
 
 #lui
 lui $3, 5
